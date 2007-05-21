cf5c44ff84c08b8e4cc16a4cb1d6ba290361b328
ajapted <ajapted@904dab67-7387-46af-8076-20c50873cc76>
2007-05-21 13:33:52 +0000

    Check-in of skeletal info-panel widget.
    git-svn-id: svn://svn.code.sf.net/p/glbsp/code/trunk@335 904dab67-7387-46af-8076-20c50873cc76

---
+++ nodeview/info.cc
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------
+//  INFO : Information Panel
+//------------------------------------------------------------------------
+//
+//  GL-Node Viewer (C) 2004-2007 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+// this includes everything we need
+#include "defs.h"
+
+
+#define INFO_BG_COLOR  fl_rgb_color(96)
+
+
+//
+// W_Info Constructor
+//
+W_Info::W_Info(int X, int Y, int W, int H, const char *label) : 
+    Fl_Group(X, Y, W, H, label)
+{
+  end();  // cancel begin() in Fl_Group constructor
+
+  box(FL_FLAT_BOX);
+
+	color(INFO_BG_COLOR, INFO_BG_COLOR);
+
+  //.... FIXME
+}
+
+//
+// W_Info Destructor
+//
+W_Info::~W_Info()
+{
+}
+
---
+++ nodeview/info.h
@@ -0,0 +1,32 @@
+//------------------------------------------------------------------------
+//  INFO : Information Panel
+//------------------------------------------------------------------------
+//
+//  GL-Node Viewer (C) 2004-2007 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __NODEVIEW_INFO_H__
+#define __NODEVIEW_INFO_H__
+
+class W_Info : public Fl_Group
+{
+public:
+	W_Info(int X, int Y, int W, int H, const char *label = 0);
+	~W_Info();
+
+public:
+
+};
+
+#endif /* __NODEVIEW_INFO_H__ */
