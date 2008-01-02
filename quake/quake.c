8ffb0a5a95b08b753f37f2ee4b293104ee5ad29a
ajapted <ajapted@904dab67-7387-46af-8076-20c50873cc76>
2008-01-02 06:02:52 +0000

    Added code for writing out player start entities.
    git-svn-id: svn://svn.code.sf.net/p/glbsp/code/trunk@465 904dab67-7387-46af-8076-20c50873cc76

--- quake/quake.c
+++ quake/quake.c
@@ -39,6 +39,38 @@
 #include "wad.h"
 
 
+static void WriteKey(FILE *fp, const char *key, const char *val)
+{
+  fprintf(fp, " \"%s\" \"%s\"\n", key, val);
+}
+
+static void WriteNumber(FILE *fp, const char *key, int val)
+{
+  fprintf(fp, " \"%s\" \"%d\"\n", key, val);
+}
+
+static void WriteTriplet(FILE *fp, const char *key, float_g x, float_g y, float_g z)
+{
+  fprintf(fp, " \"%s\" \"%1.0f %1.0f %1.0f\"\n", key, x, y, z);
+}
+
+static void WriteThing_Q3A(FILE *fp, thing_t *th)
+{
+  // player?
+  if (th->type == 1 || th->type == 11)
+  {
+    sector_t *sec = ThingFindSector(th);
+
+    fprintf(fp, "{\n");
+
+    WriteKey(fp, "classname", "info_player_deathmatch");
+    WriteNumber(fp, "angle", th->angle);
+    WriteTriplet(fp, "origin", th->x, th->y, sec->floor_h + 1 + th->dz);
+
+    fprintf(fp, "}\n");
+  }
+}
+
 static void WriteSubsec_Q3A(FILE *fp, subsec_t *sub)
 {
   // TODO
@@ -52,11 +84,27 @@ void WriteMap_Q3A(FILE *fp, node_t *root)
 {
   int i;
 
+  fprintf(fp, "// converted by AJDMQK (AJ's Doom to Quake program)\n");
+  fprintf(fp, "{\n");
+
+  WriteKey(fp, "classname", "worldspawn");
+  WriteKey(fp, "message", "an AJDMQK conversion");
+  WriteNumber(fp, "ambient", 90);
+
   for (i=0; i < num_subsecs; i++)
   {
     subsec_t *sub = LookupSubsec(i);
 
     WriteSubsec_Q3A(fp, sub);
   }
+
+  for (i=0; i < num_things; i++)
+  {
+    thing_t *th = LookupThing(i);
+
+    WriteThing_Q3A(fp, th);
+  }
+
+  fprintf(fp, "}\n");
 }
 
