c829e110e0d30be29a35edd6d2543f64cd4b5c85
ajapted <ajapted@904dab67-7387-46af-8076-20c50873cc76>
2008-02-21 01:59:44 +0000

    Import of Doom-2-Brush code.
    git-svn-id: svn://svn.code.sf.net/p/glbsp/code/trunk@476 904dab67-7387-46af-8076-20c50873cc76

---
+++ doom2brush/defs.h
@@ -0,0 +1,55 @@
+//------------------------------------------------------------------------
+//  DEFINITIONS
+//------------------------------------------------------------------------
+//
+//  Doom-2-Brush (C) 2008 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __DM2BR_DEFS_H__
+#define __DM2BR_DEFS_H__
+
+#define PROG_NAME  "Doom2Brush"
+
+//
+//  SYSTEM INCLUDES
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+
+#include <math.h>
+#include <limits.h>
+
+#ifndef WIN32
+#include <sys/time.h>
+#endif
+
+#include <vector>
+
+
+//
+//  LOCAL INCLUDES
+//
+#include "system.h"
+#include "asserts.h"
+#include "structs.h"
+#include "util.h"
+#include "wad.h"
+#include "level.h"
+
+#endif /* __DM2BR_DEFS_H__ */
